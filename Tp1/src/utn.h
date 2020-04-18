039d2cb8e53170c22509f358f0dc0ba12ff4819d
fumi1993 <47338032+fumi1993@users.noreply.github.com>
2020-04-18 19:14:55 -0300

    Add files via upload

---
+++ Tp1/src/utn.h
@@ -0,0 +1,17 @@
+/*
+ * header.h
+ *
+ *  Created on: 15 abr. 2020
+ *      Author: Asato Fumihito
+ */
+
+#ifndef UTN_H_
+#define UTN_H_
+
+//estos son los prototipos utilizados en las funciones.
+float suma(float A, float B);
+float resta(float A, float B);
+float multiplicacion(float A, float B);
+float division(float A, float B);
+float factorial(float A, float B);
+#endif /* UTN_H_ */
